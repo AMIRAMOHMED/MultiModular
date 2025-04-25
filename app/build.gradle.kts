29d651dfe1b1402ddd3fe6f50097eaa7ab05e78d
Amiramohamed <amiramohmaed887@gmail.com>
2025-04-25 17:21:46 +0300

    add FlavorTypes, BuildDimensions, and BuildTypes objects; update build.gradle.kts with new dependencies and repository settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,11 +42,11 @@ android {
 dependencies {
     implementation(Dependencies.ANDROIDX_CORE)
     implementation(Dependencies.ANDROIDX_LIFECYCLE_RUNTIME_KTX)
+    implementation(Dependencies.ANDROIDX_ACTIVITY_COMPOSE)
     implementation(Dependencies.ANDROIDX_UI)
     implementation(Dependencies.ANDROIDX_UI_GRAPHICS)
     implementation(Dependencies.ANDROIDX_UI_TOOLING_PREVIEW)
     implementation(Dependencies.ANDROIDX_MATERIAL3)
-    implementation(Dependencies.ANDROIDX_ACTIVITY_COMPOSE)
     testImplementation(TestDependencies.ANDROIDX_JUNIT)
     androidTestImplementation(TestDependencies.ANDROIDX_JUNIT)
     androidTestImplementation(TestDependencies.ANDROIDX_ESPRESSO_CORE)
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,12 @@ plugins {
 }
 
 repositories {
+    google()
     mavenCentral()
+}
+dependencies{
+    api(kotlin("gradle-plugin:2.0.21"))
+    implementation("com.android.tools.build:gradle:8.9.0")
+
+
 }
